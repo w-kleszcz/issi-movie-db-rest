3fba539956725242d388aa59e7cf1dab4acf605e
jdajda <dajda@agh.edu.pl>
2025-01-22 13:12:45 +0100

    Cleared main.py from commented code

--- main.py
+++ main.py
@@ -8,37 +8,18 @@ import models
 from database import db_state_default
 
 app = FastAPI()
-#
-# async def reset_db_state():
-#     database.db._state._state.set(db_state_default.copy())
-#     database.db._state.reset()
-#
-#
-# def get_db(db_state=Depends(reset_db_state)):
-#     try:
-#         database.db.connect()
-#         yield
-#     finally:
-#         if not database.db.is_closed():
-#             database.db.close()
 
-
-# @app.get("/movies/", response_model=List[schemas.Movie], dependencies=[Depends(get_db)])
 @app.get("/movies/", response_model=List[schemas.Movie])
 def get_movies():
     return list(models.Movie.select())
     # movies = crud.get_movies()
     # return movies
 
-# @app.get("/movies/", response_model=List[schemas.Movie], dependencies=[Depends(get_db)])
 @app.post("/movies/", response_model=schemas.Movie)
 def add_movie(movie: schemas.MovieBase):
     movie = models.Movie.create(**movie.dict())
     return movie
 
-# @app.get(
-#     "/movies/{movie_id}", response_model=schemas.Movie, dependencies=[Depends(get_db)]
-# )
 @app.get("/movies/{movie_id}", response_model=schemas.Movie)
 def get_movie(movie_id: int):
     db_movie = models.Movie.filter(models.Movie.id == movie_id).first()
